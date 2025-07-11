c983ea226ac3b05354afdda52ed145930eea9b20
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:15:18 +0530

    Create Class.cpp

---
+++ Class.cpp
@@ -0,0 +1,16 @@
+//Class and object program
+#include<iostream>
+using namespace std;
+class Sample
+{
+public:
+void Test()
+{
+cout<<"\nHello World....";
+}
+};
+int main()
+{
+Sample S;
+S.Test();
+}
