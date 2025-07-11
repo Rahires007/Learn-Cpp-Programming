104626115a8c1a27e062c8f4bde9f79b402b80e1
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:22:40 +0530

    Create IFelse.cpp

---
+++ IFelse.cpp
@@ -0,0 +1,14 @@
+//IF else conditional statement program
+#include<iostream>
+using namespace std;
+int main()
+{
+int Age;
+cout<<"\nEnter your age...";
+cin>>Age;
+if(Age>=18)
+  cout<<"\nYou are eligible for voting.....";
+else
+  cout<<"\nYou are not eligible for voting....";
+return 0;
+}
