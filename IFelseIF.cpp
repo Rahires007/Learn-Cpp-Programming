20391d017a04347aa19df0876025b82d5060eacb
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:23:23 +0530

    Create IFelseIF.cpp

---
+++ IFelseIF.cpp
@@ -0,0 +1,16 @@
+//IF else IF conditional statement
+#include<iostream>
+using namespace std;
+int main()
+{
+int Num;
+cout<<"\nEnter a no.....";
+cin>>Num;
+if(Num>0)
+  cout<<"\nGiven no is Positive......";
+else if(Num<0)
+cout<<"\nGiven no is Negative......";
+else
+  cout<<"\nGiven no is zero.....";
+return 0;
+}
