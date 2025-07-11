c11785beccf0693c4fa8a1557f39bbfc6756d4c2
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:26:31 +0530

    Create Operation.cpp

---
+++ Operation.cpp
@@ -0,0 +1,14 @@
+//All Arithmetic Operation
+#include<iostream>
+using namespace std;
+int main()
+{
+float Num1,Num2;
+cout<<"\nEnter 2 no....";
+cin>>Num1>>Num2;
+cout<<"\nAdd ="<<Num1+Num2;
+cout<<"\nSub ="<<Num1-Num2;
+cout<<"\nMulti ="<<Num1*Num2;
+cout<<"\nDiv ="<<Num1/Num2;
+return 0;
+}
