495f308566575c4a5d110238898141a85f4e8b68
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:22:07 +0530

    Create IF.cpp

---
+++ IF.cpp
@@ -0,0 +1,14 @@
+//IF conditional statement program
+#include<iostream>
+using namespace std;
+int main()
+{
+int Password;
+cout<<"\nEnter a password.....";
+cin>>Password;
+if(Password==1137)
+  cout<<"\nWelcome....";
+if(Password!=1137)
+  cout<<"\nPlease try again....";
+return 0;
+}
